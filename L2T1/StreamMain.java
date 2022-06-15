3fbf51a298954bf9345e307105360dcbe64e2ff6
yotow <lxdr@ro.ru>
2022-06-15 16:28:28 +0300

    L2-T1 is completed

---
+++ L2T1/StreamMain.java
@@ -0,0 +1,13 @@
+package L2T1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> integerStream = intList.stream();
+        integerStream.filter(x -> x > 0).filter(x -> x % 2 == 0).sorted().forEach(System.out::println);
+    }
+}
