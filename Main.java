5d55772a1eb6e8a5f615321bb7ef6ade18633ddf
yotow <lxdr@ro.ru>
2022-06-15 12:15:07 +0300

    add errorListener

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        OnTaskDoneListener listener = System.out::println;
-        Worker worker = new Worker(listener);
+        OnTaskDoneListener doneListener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(doneListener, errorListener);
         worker.start();
     }
 }
--- Worker.java
+++ Worker.java
@@ -1,18 +1,22 @@
-import java.sql.Time;
-
 public class Worker {
 
-    private final OnTaskDoneListener callback;
+    private final OnTaskDoneListener doneCallback;
+    private final OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback){
-        this.callback = callback;
+    public Worker(OnTaskDoneListener doneCallback, OnTaskErrorListener errorCallback) {
+        this.doneCallback = doneCallback;
+        this.errorCallback = errorCallback;
     }
 
-    public void start(){
+    public void start() {
         for (int i = 0; i < 100; i++) {
             try {
-                Thread.sleep(1000);
-                callback.onDone("Task" + i + " is done");
+                Thread.sleep(100);
+                if (i == 33) {
+                    errorCallback.onError("Task " + i + " is error");
+                }
+                doneCallback.onDone("Task " + i + " is done");
+
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
