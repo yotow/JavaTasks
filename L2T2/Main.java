79f28fffabd8b3f3689ebfe24659d88d65be8e69
yotow <lxdr@ro.ru>
2022-06-19 14:21:10 +0300

    L2-T2 is fixed

--- L2T2/Main.java
+++ L2T2/Main.java
@@ -8,7 +8,7 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 100_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -22,15 +22,15 @@ public class Main {
         long countMinors = personStream.filter(p -> p.getAge() < 18).count();
 
         personStream = persons.stream();
-        List<String> familyRecruit = personStream.filter(person -> person.getAge() > 18)
+        List<String> familyRecruit = personStream.filter(person -> person.getAge() >= 18)
                 .filter(person -> person.getAge() < 27)
                 .map(Person::getFamily).toList();
 
         personStream = persons.stream();
-        List<Person> workers = personStream.filter(person -> person.getAge() > 16)
-                .filter(person -> person.getAge() < 65)
-                .filter(person -> person.getEducation() != Education.ELEMENTARY)
-                .filter(person -> person.getEducation() != Education.SECONDARY)
+        List<Person> workers = personStream.filter(person -> person.getAge() >= 16)
+                .filter(person -> (person.getAge() < 65 && person.getSex() == Sex.MAN) ||
+                        (person.getAge() < 60 && person.getSex() == Sex.WOMAN))
+                .filter(person -> person.getEducation() == Education.HIGHER)
                 .sorted(Comparator.comparing(Person::getFamily)).toList();
     }
 }
