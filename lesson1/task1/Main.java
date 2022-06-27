d2cfc4006e45761bc86a5be7d9e8b989aaaad05d
yotow <lxdr@ro.ru>
2022-06-27 11:41:34 +0300

    t1-l1 is fixed

--- lesson1/task1/Main.java
+++ lesson1/task1/Main.java
@@ -3,7 +3,7 @@ package lesson1.task1;
 import java.util.function.*;
 
 public class Main {
-
+    final static Integer ERROR_CODE = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
         calc.println.accept(calc.multiply.apply(400000000, 400000000));
@@ -16,6 +16,7 @@ public class Main {
 }
 
 class Calculator {
+    final static Integer ERROR_CODE = Integer.MIN_VALUE;
     public Calculator() {
     }
 
@@ -24,12 +25,12 @@ class Calculator {
     BinaryOperator<Integer> plus = Integer::sum;
     BinaryOperator<Integer> minus = (a, b) -> a - b;
     BinaryOperator<Integer> multiply = (a, b) -> a * b;
-    BinaryOperator<Integer> divide = (a, b) -> a / b;
+    BinaryOperator<Integer> divide = (a, b) -> b == 0 ? ERROR_CODE : a / b;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> {
         if (x == Integer.MIN_VALUE)
-            return Integer.MAX_VALUE;  // abs(Integer.MIN_VALUE) > INTEGER.MAX_VALUE ("Overflow to represent absolute value of Integer.MIN_VALUE")
+            return ERROR_CODE;  // abs(Integer.MIN_VALUE) > INTEGER.MAX_VALUE ("Overflow to represent absolute value of Integer.MIN_VALUE")
         else
             return (x < 0) ? -x : x;
     };
